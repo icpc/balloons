2163faae7816a4a4c0f954e83f42bfd4d2c1c0b2
Nikita Sychev <root@nsychev.ru>
2024-11-13 22:58:52 +0500

    Fix build error

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -14,7 +14,6 @@ fun TaskContainerScope.pnpmBuild(name: String, configure: PnpmTask.(Directory) -
     outputs.cacheIf { true }
     environment.set(mapOf("BUILD_PATH" to "build"))
     inputs.dir(layout.projectDirectory.dir("src"))
-    inputs.dir(layout.projectDirectory.dir("public"))
     inputs.file(layout.projectDirectory.file("package.json"))
     inputs.file(layout.projectDirectory.file("pnpm-lock.yaml"))
     inputs.file(layout.projectDirectory.file("tsconfig.json"))
