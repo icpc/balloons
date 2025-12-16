a69c0d4b5d015995bd1762b846d7402c665ec585
Koloskov Konstantin <408827@niuitmo.ru>
2025-12-16 20:13:17 +0300

    add required balloons counter

--- frontend/src/pages/ActiveBalloons.tsx
+++ frontend/src/pages/ActiveBalloons.tsx
@@ -9,96 +9,87 @@ import { RootState } from '../store/store';
 import { useWebSocket } from '../contexts/WebSocketContext';
 import { useFilteredBalloons } from '../hooks/useFilteredBalloons';
 import { useTranslation } from 'react-i18next';
-import { useContestMaps } from '../hooks/useContestMaps';
-import { Balloon, Team } from '../types';
-
-function teamId(balloon: Balloon, teamMap: Record<string, Team>): string {
-  return teamMap[balloon.teamId]?.displayName ?? balloon.teamId;
-}
 
 const BalloonsView = ({ infoHolder }: { infoHolder: InfoHolder }) => {
-  const { t } = useTranslation();
-  const ws = useWebSocket();
-  const contest = useSelector((state: RootState) => state.contest);
-  const filteredBalloons = useFilteredBalloons();
-  const { teamMap } = useContestMaps(contest);
+    const { t } = useTranslation();
+    const ws = useWebSocket();
+    const contest = useSelector((state: RootState) => state.contest);
+    const filteredBalloons = useFilteredBalloons();
 
-  const myBalloons = useMemo(() => {
-    return filteredBalloons
-      .filter(balloon => balloon.takenBy === infoHolder.info.login && !balloon.delivered)
-      .sort((a, b) => teamId(a, teamMap).localeCompare(teamId(b, teamMap)));
-  }, [filteredBalloons, infoHolder.info.login, teamMap]);
+    const myBalloons = useMemo(() => {
+        return filteredBalloons.filter(balloon => balloon.takenBy === infoHolder.info.login && !balloon.delivered);
+    }, [filteredBalloons, infoHolder.info.login]);
 
-  const queuedBalloons = useMemo(() => {
-    return filteredBalloons.filter(balloon => balloon.takenBy === null && !balloon.delivered);
-  }, [filteredBalloons]);
+    const queuedBalloons = useMemo(() => {
+        return filteredBalloons.filter(balloon => balloon.takenBy === null && !balloon.delivered);
+    }, [filteredBalloons]);
 
-  const takenBalloons = useMemo(() => {
-    return filteredBalloons.filter(balloon => balloon.takenBy !== null && !balloon.delivered);
-  }, [filteredBalloons]);
+    const takenBalloons = useMemo(() => {
+        return filteredBalloons.filter(balloon => balloon.takenBy !== null && !balloon.delivered);
+    }, [filteredBalloons]);
 
-  return (
-    <main className="balloons-main">
-      <h2 className="sr-only">{t('balloons.title')}</h2>
-      <div className="contest-name"><strong>{contest.name}</strong></div>
-      <ProblemList contest={contest} balloons={filteredBalloons} />
-      <BalloonList
-        title={t('balloons.youCarry')}
-        balloons={myBalloons}
-        contest={contest}
-        actions={balloon => (
-          <>
-            <a onClick={() => ws?.send(JSON.stringify({ type: 'deliverBalloon', runId: balloon.runId }))}>
-              {t('balloons.actions.done')}
-            </a>
-            <a onClick={() => ws?.send(JSON.stringify({ type: 'dropBalloon', runId: balloon.runId }))}>
-              {t('balloons.actions.drop')}
-            </a>
-          </>
-        )}
-      />
-      <BalloonList
-        title={t('balloons.available')}
-        balloons={queuedBalloons}
-        contest={contest}
-        actions={balloon => (
-          <a onClick={() => ws?.send(JSON.stringify({ type: 'takeBalloon', runId: balloon.runId }))}>
-            {t('balloons.actions.take')}
-          </a>
-        )}
-      />
-      <BalloonList
-        title={t('balloons.inProgress')}
-        balloons={takenBalloons}
-        contest={contest}
-        actions={balloon => (
-          <span>
+    return (
+        <main className="balloons-main">
+            <h2 className="sr-only">{t('balloons.title')}</h2>
+            <div className="contest-name"><strong>{contest.name}</strong></div>
+            <ProblemList contest={contest} balloons={filteredBalloons}/>
+            <BalloonList
+                title={t('balloons.youCarry')}
+                balloons={myBalloons}
+                contest={contest}
+                actions={balloon => (
+                    <>
+                        <a onClick={() => ws?.send(JSON.stringify({ type: 'deliverBalloon', runId: balloon.runId }))}>
+                            {t('balloons.actions.done')}
+                        </a>
+                        <a onClick={() => ws?.send(JSON.stringify({ type: 'dropBalloon', runId: balloon.runId }))}>
+                            {t('balloons.actions.drop')}
+                        </a>
+                    </>
+                )}
+            />
+            <BalloonList
+                title={t('balloons.available')}
+                balloons={queuedBalloons}
+                contest={contest}
+                actions={balloon => (
+                    <a onClick={() => ws?.send(JSON.stringify({ type: 'takeBalloon', runId: balloon.runId }))}>
+                        {t('balloons.actions.take')}
+                    </a>
+                )}
+            />
+            <BalloonList
+                title={t('balloons.inProgress')}
+                balloons={takenBalloons}
+                contest={contest}
+                actions={balloon => (
+                    <span>
             {t('balloons.carriedBy')}
-            {balloon.takenBy}
+                        {balloon.takenBy}
           </span>
-        )}
-      />
-    </main>
-  );
+                )}
+            />
+        </main>
+    );
 };
 
 const ActiveBalloons = ({ infoHolder }: { infoHolder: InfoHolder }) => {
-  const { t } = useTranslation();
+    const { t } = useTranslation();
 
-  if (!infoHolder.info.login) {
-    return <Navigate to="/login" />;
-  }
+    if (!infoHolder.info.login) {
+        return <Navigate to="/login" />;
+    }
 
-  if (!infoHolder.info.canAccess) {
-    return (
-      <GlobalError
-        title={t('errors.noAccess')}
-        message={t('errors.contactAdmin')}
-      />
-    );
-  }
+    if (!infoHolder.info.canAccess) {
+        return (
+            <GlobalError
+                title={t('errors.noAccess')}
+                message={t('errors.contactAdmin')}
+            />
+        );
+    }
 
-  return <BalloonsView infoHolder={infoHolder} />;
+    return <BalloonsView infoHolder={infoHolder} />;
 };
 
 export default ActiveBalloons;
